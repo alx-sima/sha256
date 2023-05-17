abd5b29f93632a1fc9c2e4f3b1b64682c7b8436b
Alexandru Sima <60713563+alx-sima@users.noreply.github.com>
2023-05-17 16:59:56 +0300

    added functions for file reading

---
+++ io.c
@@ -0,0 +1,80 @@
+/* Copyright 2023 Alexandru Sima & Iarina Popa */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "io.h"
+
+/**
+ * @brief Citeste input de la `stdin`.
+ *
+ * @return stringul citit pana la intalnirea EOF (C-d)
+ * @retval NULL eroare la citire (lipsa memorie)
+ */
+static char *read_stdin();
+
+/**
+ * @brief Calculeaza dimensiunea unui fisier.
+ *
+ * @param fstream descriptorul fisierului
+ * @retval numarul de caractere stocate in fisier
+ */
+static inline long get_file_size(FILE *fstream);
+
+char *read_file_contents(char *filename)
+{
+	/* `filename`-ul "-" inseamna ca citirea se face de la stdin. */
+	if (!strcmp(filename, "-"))
+		return read_stdin();
+
+	FILE *fstream = fopen(filename, "rb");
+	if (!fstream)
+		return NULL;
+
+	fseek(fstream, 0, SEEK_END);
+	long file_size = get_file_size(fstream);
+
+	char *contents = malloc(sizeof(char) * file_size);
+	if (!contents) {
+		fclose(fstream);
+		return NULL;
+	}
+
+	fread(contents, sizeof(char), file_size, stdin);
+	fclose(fstream);
+	return contents;
+}
+
+static char *read_stdin()
+{
+	char buffer[BUFSIZ];
+	char *read_bytes = NULL;
+	size_t len_bytes = 0;
+
+	while (fgets(buffer, BUFSIZ, stdin)) {
+		size_t batch_size = strlen(buffer);
+
+		char *tmp_buf = realloc(read_bytes, len_bytes + batch_size);
+		if (!tmp_buf) {
+			free(read_bytes);
+			return NULL;
+		}
+
+		read_bytes = tmp_buf;
+		strncpy(read_bytes + len_bytes, buffer, batch_size);
+	}
+
+	return read_bytes;
+}
+
+static inline long get_file_size(FILE *fstream)
+{
+	long curr_pos = ftell(fstream);
+	fseek(fstream, 0, SEEK_END);
+	long size = ftell(fstream);
+
+	/* Restabileste pozitia anterioara a cursorului. */
+	fseek(fstream, curr_pos, SEEK_SET);
+
+	return size;
+}
---
+++ io.h
@@ -0,0 +1,15 @@
+/* Copyright 2023 Alexandru Sima & Iarina Popa */
+#ifndef IO_H_
+#define IO_H_
+
+/**
+ * @brief Citeste si intoarce continutul unui fisier/de la tastatura.
+ *
+ * @param filename calea fisierului citit. calea speciala "-" inseamna STDIN
+ *
+ * @return      continutul fisierului
+ * @retval NULL a intervenit o eroare la citire
+ */
+char *read_file_contents(char *filename);
+
+#endif /* IO_H_ */
